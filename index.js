0aa7c5e0260d270f43ccdaa2fcad16cd9e1d4758
Lincoln <lincoln@taylorlivingco.com>
2022-07-31 20:07:19 +1000

    complete assignment

---
+++ index.js
@@ -0,0 +1,64 @@
+function add(a, b) 
+{
+    return a + b;
+}
+
+console.log(add(5,2));
+
+
+function subtract(a, b)
+{
+    return a - b;
+}
+
+console.log(subtract(10,2));
+
+
+
+function multiply(a, b)
+{
+    return a * b;
+}
+
+console.log(multiply(2,2));
+
+
+
+function divide(a, b)
+{
+    return a / b;
+}
+
+console.log(divide(2,2));
+
+
+
+function increment(n)
+{
+    return ++n;
+}
+
+console.log(increment(2));
+
+
+function decrement(n)
+{
+    return --n;
+}
+
+console.log(decrement(2));
+
+
+function makeInt(n)
+{
+    return parseInt(n, 10);
+}
+
+console.log(makeInt(2.677));
+
+function preserveDecimal(n)
+{
+    return parseFloat(n);
+}
+
+console.log(preserveDecimal(2.4344656546));
